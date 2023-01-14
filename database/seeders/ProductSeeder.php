4915f93fb162fd2a16152689e7ee7470daace942
Francesca-Barcella <pollon.fra@gmail>
2023-01-14 17:38:25 +0100

    popolato database

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = config('products.products');
+        //dd($products);
+
+        foreach ($products as $product) {
+            $new_product = new Product();
+            $new_product->src = $product['src'];
+            $new_product->title = $product['titolo'];
+            $new_product->description = $product['descrizione'];
+            $new_product->cooking_time = $product['cottura'];
+            $new_product->type = $product['tipo'];
+            $new_product->weight = $product['peso'];
+            $new_product->save();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::get('/about', [PageController::class, 'about'])->name('about');
+
 
 Route::get('/products', function () {
     return view('products');
