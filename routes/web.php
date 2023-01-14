305da5ebc779bd75253319f2df9f1968f74ff692
Francesca-Barcella <pollon.fra@gmail>
2023-01-14 16:21:27 +0100

    installazioni & import layout molisana

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
